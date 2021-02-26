443df5a09c0e395db07153f22a3157976844b230
Dr. Brain <info@rightbraingroup.com>
2021-02-26 16:12:37 -0500

    Initial commit

---
+++ components/AppNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li>
+        <nuxt-link exact to="/">
+          <app-icon></app-icon>DigDat
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/archive">Archive</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/contact">Contact</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import AppIcon from "@/components/AppIcon.vue";
+
+export default {
+  components: {
+    AppIcon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  background: #000;
+  color: #fff;
+  padding: 1em 100px;
+
+  a {
+    color: #fff;
+    opacity: 0.8;
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+
+div {
+  align-items: center;
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+ul {
+  padding-left: 0;
+
+  li {
+    display: inline-block;
+    margin-right: 30px;
+  }
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>Contact</h1>
+    <p>
+        this will be a form
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.about {
+  margin: 30px auto 50px;
+  max-width: 800px;
+  padding: 50px 50px 70px;
+  background: white;
+}
+
+p {
+  margin: 15px 0 20px;
+}
+</style>
